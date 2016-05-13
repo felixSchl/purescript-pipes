cbba0b9b495a0d80980db7f321a78ac598bd8fd3
Felix Schlitter <felixschlitter@gmail.com>
2016-05-13 12:40:23 +1200

    Initial commit

---
+++ src/Pipes/Internal.purs
@@ -0,0 +1,52 @@
+module Pipes.Internal where
+
+import Prelude
+import Data.Monoid (class Monoid, mempty)
+import Control.Monad.Trans (class MonadTrans)
+
+data Proxy a' a b' b m r
+  = Request a' (a  -> Proxy a' a b' b m r)
+  | Respond b  (b' -> Proxy a' a b' b m r)
+  | M          (m    (Proxy a' a b' b m r))
+  | Pure       r
+
+instance functorProxy :: (Monad m) => Functor (Proxy a' a b' b m) where
+  map f p0 = go p0 where
+    go p = case p of
+      Request a' fa  -> Request a' (go <<< fa)
+      Respond b  fb' -> Respond b  (go <<< fb')
+      M           m  -> M          (go <$> m)
+      Pure        r  -> Pure       (f r)
+
+instance applyProxy :: (Monad m) => Apply (Proxy a' a b' b m) where
+  apply pf0 px = go pf0 where
+    go pf = case pf of
+      Request a' fa  -> Request a' (go <<< fa)
+      Respond b  fb' -> Respond b  (go <<< fb')
+      M           m  -> M          (go <$> m)
+      Pure        f  -> f <$> px
+
+instance applicativeProxy :: (Monad m) => Applicative (Proxy a' a b' b m) where
+  pure = Pure
+
+instance bindProxy :: (Monad m) => Bind (Proxy a' a b' b m) where
+  bind p0 f = go p0 where
+    go p = case p of
+      Request a' fa  -> Request a' (go <<< fa)
+      Respond b  fb' -> Respond b  (go <<< fb')
+      M           m  -> M          (go <$> m)
+      Pure        r  -> f r
+
+instance monoidProxy :: (Monad m, Monoid r) => Monoid (Proxy a' a b' b m r) where
+  mempty = Pure mempty
+
+instance semigroupProxy :: (Monad m, Semigroup r) => Semigroup (Proxy a' a b' b m r) where
+  append p1 p2 = go p1 where
+    go p = case p of
+      Request a' fa  -> Request a' (go <<< fa)
+      Respond b  fb' -> Respond b  (go <<< fb')
+      M           m  -> M          (go <$> m)
+      Pure       r1  -> (r1 <> _) <$> p2
+
+instance monadTransProxy :: MonadTrans (Proxy a' a b' b) where
+  lift m = M (Pure <$> m)
