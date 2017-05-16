9d2e86a8fdecbbe6294bd53f3b2da49f957d210f
Dmitry Kouznetsov <dmitry.kouznetsov@gmail.com>
2017-05-16 19:01:54 +0200

    Make `foo` even more generic on m

--- examples/102.purs
+++ examples/102.purs
@@ -2,11 +2,11 @@ module Main where
 
 import Prelude
 import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 
-import Control.Monad.Trans.Class (lift)
-import Pipes (for, yield)
-import Pipes.Core (runEffect, Producer_)
+import Pipes (yield)
+import Pipes.Core ((//>), runEffect, Producer_)
 
 type Result = Int
 
@@ -19,16 +19,16 @@ myLog = yield
 
 foo
   :: forall eff
-   . Int
+   . Int  
   -> Int
   -> Producer_ String (Eff eff) Result
 foo x y = do
   myLog $ "About to compute: " <> show x <> " + " <> show y
-  r <- lift $ pure $ x + y
+  r <- pure $ x + y
   myLog $ "Computed: " <> show x <> " + " <> show y <> " = " <> show r
-  lift $ pure r
+  pure r
 
 main :: Eff (console :: CONSOLE) Unit
 main = do
-  r <- runEffect $ for (foo 1 2) (lift <<< log <<< append "Log >>> ")
+  r <- runEffect $ (foo 1 2) //> (liftEff <<< log <<< append "Log >>> ")
   log $ "Result >>> " <> show r
