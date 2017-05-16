01a7d680c126193b9ea41dd338d2cbd7b0a24192
Dmitry Kouznetsov <dmitry.kouznetsov@gmail.com>
2017-05-16 17:20:31 +0200

    Making an example for how to use Pipes

---
+++ examples/102.purs
@@ -0,0 +1,33 @@
+module Main where
+
+import Prelude
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE, log)
+
+import Control.Monad.Trans.Class (lift)
+import Pipes (for, yield)
+import Pipes.Core (runEffect, Producer_)
+
+type Result = Int
+
+myLog
+  :: forall m
+   . Monad m
+  => String
+  -> Producer_ String m Unit
+myLog = yield
+
+foo
+  :: Int
+  -> Int
+  -> Producer_ String (Eff (console :: CONSOLE)) Result
+foo x y = do
+  myLog $ "About to compute: " <> show x <> " + " <> show y
+  r <- lift $ pure $ x + y
+  myLog $ "Computed: " <> show x <> " + " <> show y <> " = " <> show r
+  lift $ pure r
+
+main :: Eff (console :: CONSOLE) Unit
+main = do
+  r <- runEffect $ for (foo 1 2) (lift <<< log <<< append "Log >>> ")
+  log $ "Result >>> " <> show r
